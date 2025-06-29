33aaaeced60c691b2d3a34ae824b8fc0a220bd44
adnan8474 <adnanalrefai@gmail.com>
2025-06-29 13:24:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
-    export default {
+    port: 5173,
+  },
   build: {
     rollupOptions: {
-      external: ['/logo.png']
-    }
-  }
-}
-  }
+      external: ['/logo.png'],
+    },
+  },
 });
